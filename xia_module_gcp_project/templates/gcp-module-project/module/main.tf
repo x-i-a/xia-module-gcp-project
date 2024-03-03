c24dbb5905531ca58e2cd93bc067a85f01c087ff
X-I-A <develop@x-i-a.com>
2024-03-03 20:01:19 +0100

    feat: changing state file configuration

--- xia_module_gcp_project/templates/gcp-module-project/module/main.tf
+++ xia_module_gcp_project/templates/gcp-module-project/module/main.tf
@@ -9,6 +9,7 @@ terraform {
 locals {
   project = yamldecode(file(var.project_file))
   landscape = yamldecode(file(var.landscape_file))
+  tf_bucket_name = lookup(lookup(local.landscape, "settings", {}), "cosmos_name")
   applications = yamldecode(file(var.applications_file))
   folder_id = lookup(local.project, "folder_id", null)
   project_prefix = local.project["project_prefix"]
@@ -58,15 +59,6 @@ resource "google_project_service" "identity_and_access_manager_api" {
   project = google_project.env_projects[each.key].project_id
   service = "iam.googleapis.com"
   disable_on_destroy = false
-}
-
-resource "google_storage_bucket" "tfstate-bucket" {
-  for_each = { for s in local.all_pool_settings : "${s.app_name}-${s.env_name}" => s }
-
-  project       = local.landscape["settings"]["cosmos_name"]
-  name          = "${local.landscape["settings"]["realm_name"]}_${each.value["app_name"]}_${each.value["env_name"]}"
-  location      = local.landscape["settings"]["foundation_region"]
-  force_destroy = true
 }
 
 resource "google_iam_workload_identity_pool" "github_pool" {
@@ -133,7 +125,7 @@ resource "google_service_account_iam_binding" "workload_identity_binding" {
 
 resource "google_storage_bucket_iam_member" "tfstate_bucket_assign" {
   for_each = { for s in local.all_pool_settings : "${s.app_name}-${s.env_name}" => s }
-  bucket = google_storage_bucket.tfstate-bucket[each.key].id
+  bucket = local.tf_bucket_name
   role   = "roles/storage.admin"
   member = "serviceAccount:${google_service_account.github_provider_sa[each.key].email}"
 }
@@ -181,5 +173,5 @@ resource "github_actions_environment_variable" "action_var_tf_bucket" {
   repository       = each.value["repository_name"]
   environment      = each.value["env_name"]
   variable_name    = "TF_BUCKET_NAME"
-  value            = google_storage_bucket.tfstate-bucket[each.key].id
+  value            = local.tf_bucket_name
 }
