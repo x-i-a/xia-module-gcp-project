7c9ae2222a618c6e6a54f52d84f1ff1c17ba3ee3
X-I-A <develop@x-i-a.com>
2024-04-06 17:45:25 +0200

    feat: explict module declaration

--- xia_module_gcp_project/__init__.py
+++ xia_module_gcp_project/__init__.py
@@ -1,7 +1,12 @@
 from xia_module_gcp_project.project import Project
 
+modules = {
+    "gcp-module-project": "Project"
+}
+
+
 __all__ = [
     "Project"
 ]
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
