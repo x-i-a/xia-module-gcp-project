d33c8793463731a9a25079576a6d8dbd7624a74a
X-I-A <develop@x-i-a.com>
2024-04-08 21:00:23 +0200

    feat: Separate Terraform State Part

--- xia_module_gcp_project/templates/gcp-module-project/module/output.tf
+++ xia_module_gcp_project/templates/gcp-module-project/module/output.tf
@@ -1,3 +1,3 @@
 output "github_provider_sa" {
-  value = google_service_account.github_provider_sa
+  value = { for key, sa in google_service_account.github_provider_sa : key => { email = sa.email } }
 }
