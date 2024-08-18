6603180485024b0c64d237fd4861442e09f0168f
X-I-A <develop@x-i-a.com>
2024-08-18 20:06:05 +0200

    feat: defining dependencies in the configuration file

--- xia_module_gcp_project/templates/gcp-module-project/base/main.tf
+++ xia_module_gcp_project/templates/gcp-module-project/base/main.tf
@@ -5,5 +5,6 @@ module "gcp_module_project" {
   landscape_file = "../../../config/landscape.yaml"
   applications_file = "../../../config/applications.yaml"
 
+  app_env_config = local.app_env_config
   depends_on = [module.gh_module_application]
 }
