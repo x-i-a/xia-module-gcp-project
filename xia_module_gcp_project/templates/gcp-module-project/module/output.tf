c0725278912036f6cc979c6d13935ed539313db8
X-I-A <develop@x-i-a.com>
2024-08-18 21:53:25 +0200

    feat: defining dependencies in the configuration file

--- xia_module_gcp_project/templates/gcp-module-project/module/output.tf
+++ xia_module_gcp_project/templates/gcp-module-project/module/output.tf
@@ -1,3 +1,7 @@
+output "gcp_projects" {
+  value = { for key, project in google_project.env_projects : key => { project_id = project.project_id } }
+}
+
 output "github_provider_sa" {
   value = { for key, sa in google_service_account.github_provider_sa : key => { email = sa.email } }
 }
