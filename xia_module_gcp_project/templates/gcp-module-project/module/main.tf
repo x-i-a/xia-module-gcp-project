a328adc813d0f0523e22a250d9a03db51fe03988
X-I-A <develop@x-i-a.com>
2024-08-19 09:06:31 +0200

    feat: defining dependencies in the configuration file

--- xia_module_gcp_project/templates/gcp-module-project/module/main.tf
+++ xia_module_gcp_project/templates/gcp-module-project/module/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+    }
+  }
+}
+
 locals {
   applications = var.applications
   settings = lookup(var.landscape, "settings", {})
