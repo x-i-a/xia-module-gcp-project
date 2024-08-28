ed49913a15a106517de52828be8c2c5a491a5305
X-I-A <develop@x-i-a.com>
2024-08-28 21:07:05 +0200

    feat: adding config files

--- xia_module_gcp_project/templates/gcp-module-organization/module/main.tf
+++ xia_module_gcp_project/templates/gcp-module-organization/module/main.tf
@@ -13,12 +13,12 @@ locals {
   module_name = coalesce(var.module_name, basename(path.module))
   landscape = var.landscape
   settings = lookup(local.landscape, "settings", {})
+  cosmos_name = local.settings["cosmos_name"]
 }
 
 locals {
   org_config = yamldecode(file(var.config_file))
   cosmos_org = local.org_config["cosmos_org"]
-  cosmos_name = local.org_config["cosmos_name"]
   cosmos_project = local.org_config["cosmos_project"]
 }
 
